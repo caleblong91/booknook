6d8bbebca45f9f6314899c15f298375d6939c6ce
caleblong91 <caleb.long@scriptdrop.co>
2023-08-03 16:08:00 -0400

    Created landing screen with logo

--- booknook/ContentView.swift
+++ booknook/ContentView.swift
@@ -6,21 +6,29 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.init(red: 252.0, green: 252.0, blue: 252.0, alpha: 1.0)).ignoresSafeArea()
+            
+            VStack{
+                Image("logo").resizable().cornerRadius(15).aspectRatio(contentMode: .fit)
+            }
         }
-        .padding()
+        
+      
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
+    
 }
+
+
