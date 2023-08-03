81ce847756f1905e08097b1a95ba1cc227598b83
caleblong91 <caleb.long@scriptdrop.co>
2023-08-03 09:39:59 -0400

    Initial Commit

---
+++ booknook/booknookApp.swift
@@ -0,0 +1,17 @@
+//
+//  booknookApp.swift
+//  booknook
+//
+//  Created by Caleb Long on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct booknookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
